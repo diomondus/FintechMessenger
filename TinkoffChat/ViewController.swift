b50a9c7587e970273ef9e5a99c786a2726e66b33
Dmitry Butilov <acebutilov@gmail.com>
2017-03-19 20:48:38 +0300

    TFS. DZ2 (no cool)

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -9,7 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ColoredLabel: UILabel!
 
+    @IBOutlet weak var BlackButton: UIButton!
+    @IBOutlet weak var RedButton: UIButton!
+    @IBOutlet weak var GreenButton: UIButton!
+    @IBOutlet weak var BlueButton: UIButton!
+    @IBOutlet weak var PinkButton: UIButton!
+    
+    @IBAction func paintTextToBlack(_ sender: Any) {
+        ColoredLabel.textColor = UIColor.black
+    }
+    
+    @IBAction func paintTextToRed(_ sender: Any) {
+         ColoredLabel.textColor = UIColor.red
+    }
+    
+    @IBAction func paintTextToGreen(_ sender: Any) {
+        ColoredLabel.textColor = UIColor.green
+    }
+    
+    @IBAction func paintTextToBlue(_ sender: Any) {
+        ColoredLabel.textColor = UIColor.blue
+    }
+    
+    @IBAction func paintTextToPurple(_ sender: Any) {
+        ColoredLabel.textColor = UIColor.purple
+    }
+    
+    @IBAction func saveProfileAction(_ sender: Any) {
+        print("Сохранение данных профиля")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +51,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
